61c2b62706e6db895144733bdcccf16f8db29a41
tschwarze <tschwarze@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2003-05-14 10:07:56 +0000

    fix indentation according to coding style guide
    git-svn-id: http://svn.codehaus.org/izpack@395 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- izpack-src/branches/Arjuna/src/lib/com/izforge/izpack/panels/PacksPanel.java
+++ izpack-src/branches/Arjuna/src/lib/com/izforge/izpack/panels/PacksPanel.java
@@ -95,19 +95,19 @@ public class PacksPanel extends IzPanel implements ActionListener,ListSelectionL
 
     add(Box.createRigidArea(new Dimension(0, 5)));
 
-	packsTable = new JTable();
-	packsTable.setIntercellSpacing(new Dimension(0,0));
+    packsTable = new JTable();
+    packsTable.setIntercellSpacing(new Dimension(0,0));
     packsTable.setBackground(Color.white);
     packsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-	packsTable.getSelectionModel().addListSelectionListener(this);
-	packsTable.setShowGrid(false);
-	tableScroller = new JScrollPane(packsTable);
+    packsTable.getSelectionModel().addListSelectionListener(this);
+    packsTable.setShowGrid(false);
+    tableScroller = new JScrollPane(packsTable);
     tableScroller.setAlignmentX(LEFT_ALIGNMENT);
     tableScroller.getViewport().setBackground(Color.white);
     tableScroller.setPreferredSize(new Dimension(300,(idata.guiPrefs.height/3+30)));
-	add(tableScroller);
+    add(tableScroller);
 
-	descriptionArea = new JTextArea();
+    descriptionArea = new JTextArea();
     descriptionArea.setMargin(new Insets(2,2,2,2));
     descriptionArea.setAlignmentX(LEFT_ALIGNMENT);
     descriptionArea.setCaretPosition(0);
@@ -116,14 +116,14 @@ public class PacksPanel extends IzPanel implements ActionListener,ListSelectionL
     descriptionArea.setOpaque(false);
     descriptionArea.setLineWrap(true);
     descriptionArea.setWrapStyleWord(true);
-	descriptionArea.setBorder(BorderFactory.createTitledBorder(parent.langpack.getString("PacksPanel.description")));
+    descriptionArea.setBorder(BorderFactory.createTitledBorder(parent.langpack.getString("PacksPanel.description")));
     add(descriptionArea);
 
-	JPanel spacePanel = new JPanel();
+    JPanel spacePanel = new JPanel();
     spacePanel.setAlignmentX(LEFT_ALIGNMENT);
-	spacePanel.setLayout(new BoxLayout(spacePanel, BoxLayout.X_AXIS));
-	spacePanel.add(new JLabel(parent.langpack.getString("PacksPanel.space")));
-	spacePanel.add(Box.createHorizontalGlue());
+    spacePanel.setLayout(new BoxLayout(spacePanel, BoxLayout.X_AXIS));
+    spacePanel.add(new JLabel(parent.langpack.getString("PacksPanel.space")));
+    spacePanel.add(Box.createHorizontalGlue());
     spaceLabel = new JLabel("");
 //    spaceLabel.setFont(new Font("Monospaced",Font.PLAIN,11));
     spacePanel.add(spaceLabel);
@@ -134,59 +134,59 @@ public class PacksPanel extends IzPanel implements ActionListener,ListSelectionL
   /**  Called when the panel becomes active.  */
   public void panelActivate()
   {
-	try
-	{
-		packsTable.setModel(new PacksModel(idata.availablePacks, idata.selectedPacks));
-		CheckBoxEditorRenderer packSelectedRenderer = new CheckBoxEditorRenderer(false);
-		packsTable.getColumnModel().getColumn(0).setCellRenderer(packSelectedRenderer);
-		CheckBoxEditorRenderer packSelectedEditor = new CheckBoxEditorRenderer(true);
-		packsTable.getColumnModel().getColumn(0).setCellEditor(packSelectedEditor);
-		packsTable.getColumnModel().getColumn(0).setMaxWidth(40);
-		DefaultTableCellRenderer renderer1 = new DefaultTableCellRenderer(){
-			public void setBorder(Border b)
-			{
-			}
-		};
-		packsTable.getColumnModel().getColumn(1).setCellRenderer(renderer1);
-		DefaultTableCellRenderer renderer2 = new DefaultTableCellRenderer(){
-			public void setBorder(Border b)
-			{
-			}
+    try
+    {
+      packsTable.setModel(new PacksModel(idata.availablePacks, idata.selectedPacks));
+      CheckBoxEditorRenderer packSelectedRenderer = new CheckBoxEditorRenderer(false);
+      packsTable.getColumnModel().getColumn(0).setCellRenderer(packSelectedRenderer);
+      CheckBoxEditorRenderer packSelectedEditor = new CheckBoxEditorRenderer(true);
+      packsTable.getColumnModel().getColumn(0).setCellEditor(packSelectedEditor);
+      packsTable.getColumnModel().getColumn(0).setMaxWidth(40);
+      DefaultTableCellRenderer renderer1 = new DefaultTableCellRenderer(){
+        public void setBorder(Border b)
+        {
+        }
+      };
+      packsTable.getColumnModel().getColumn(1).setCellRenderer(renderer1);
+      DefaultTableCellRenderer renderer2 = new DefaultTableCellRenderer(){
+        public void setBorder(Border b)
+        {
+        }
 
-//			public void setFont(Font f)
-//			{
-//				super.setFont(new Font("Monospaced",Font.PLAIN,11));
-//			}
-		};
-		renderer2.setHorizontalAlignment(JLabel.RIGHT);
-		packsTable.getColumnModel().getColumn(2).setCellRenderer(renderer2);
-		packsTable.getColumnModel().getColumn(2).setMaxWidth(100);
+        //          public void setFont(Font f)
+        //          {
+        //              super.setFont(new Font("Monospaced",Font.PLAIN,11));
+        //          }
+      };
+      renderer2.setHorizontalAlignment(JLabel.RIGHT);
+      packsTable.getColumnModel().getColumn(2).setCellRenderer(renderer2);
+      packsTable.getColumnModel().getColumn(2).setMaxWidth(100);
 
-		//remove header,so we don't need more strings
-	    tableScroller.remove(packsTable.getTableHeader());
-	    tableScroller.setColumnHeaderView(null);
-	    tableScroller.setColumnHeader(null);
+      //remove header,so we don't need more strings
+      tableScroller.remove(packsTable.getTableHeader());
+      tableScroller.setColumnHeaderView(null);
+      tableScroller.setColumnHeader(null);
 
-		// set the JCheckBoxes to the currently selected panels. The selection meight have changes in another panel
-		java.util.Iterator iter = idata.availablePacks.iterator();
-		bytes = 0;
-		while (iter.hasNext())
-		{
-			Pack p = (Pack) iter.next();
-			if (p.required)
-			{
-				bytes += p.nbytes;
-				continue;
-			}
-			if (idata.selectedPacks.contains(p))
-				bytes += p.nbytes;
-		}
-	}
-	catch (Exception e)
-	{
-		e.printStackTrace();
-	}
-	showSpaceRequired();
+      // set the JCheckBoxes to the currently selected panels. The selection meight have changes in another panel
+      java.util.Iterator iter = idata.availablePacks.iterator();
+      bytes = 0;
+      while (iter.hasNext())
+      {
+        Pack p = (Pack) iter.next();
+        if (p.required)
+        {
+          bytes += p.nbytes;
+          continue;
+        }
+        if (idata.selectedPacks.contains(p))
+          bytes += p.nbytes;
+      }
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+    showSpaceRequired();
   }
 
 
@@ -228,184 +228,184 @@ public class PacksPanel extends IzPanel implements ActionListener,ListSelectionL
     new PacksPanelAutomationHelper().makeXMLData(idata, panelRoot);
   }
 
-	private class PacksModel extends AbstractTableModel
-	{
-		private List packs;
-		private List packsToInstall;
-		public PacksModel(List packs,List packsToInstall)
-		{
-			this.packs = packs;
-			this.packsToInstall = packsToInstall;
-		}
+    private class PacksModel extends AbstractTableModel
+    {
+        private List packs;
+        private List packsToInstall;
+        public PacksModel(List packs,List packsToInstall)
+        {
+            this.packs = packs;
+            this.packsToInstall = packsToInstall;
+        }
 
-			/*
-		 * @see TableModel#getRowCount()
-		 */
-		public int getRowCount()
-		{
-			return packs.size();
-		}
+            /*
+         * @see TableModel#getRowCount()
+         */
+        public int getRowCount()
+        {
+            return packs.size();
+        }
 
-		/*
-		 * @see TableModel#getColumnCount()
-		 */
-		public int getColumnCount()
-		{
-			return 3;
-		}
+        /*
+         * @see TableModel#getColumnCount()
+         */
+        public int getColumnCount()
+        {
+            return 3;
+        }
 
-		/*
-		 * @see TableModel#getColumnClass(int)
-		 */
-		public Class getColumnClass(int columnIndex)
-		{
-			switch(columnIndex)
-			{
-				case 0:
-					return Integer.class;
+        /*
+         * @see TableModel#getColumnClass(int)
+         */
+        public Class getColumnClass(int columnIndex)
+        {
+            switch(columnIndex)
+            {
+                case 0:
+                    return Integer.class;
 
-				default :
-					return String.class;
-			}
-		}
+                default :
+                    return String.class;
+            }
+        }
 
-		/*
-		 * @see TableModel#isCellEditable(int, int)
-		 */
-		public boolean isCellEditable(int rowIndex, int columnIndex)
-		{
-		    Pack pack = (Pack) packs.get(rowIndex);
-		    if (pack.required)
-		    {
-		    	return false;
-		    }
-		    else if (columnIndex == 0)
-		    {
-		    	return true;
-		    }
-		    else
-		    {
-				return false;
-		    }
-		}
+        /*
+         * @see TableModel#isCellEditable(int, int)
+         */
+        public boolean isCellEditable(int rowIndex, int columnIndex)
+        {
+            Pack pack = (Pack) packs.get(rowIndex);
+            if (pack.required)
+            {
+                return false;
+            }
+            else if (columnIndex == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-		/*
-		 * @see TableModel#getValueAt(int, int)
-		 */
-		public Object getValueAt(int rowIndex, int columnIndex)
-		{
-		    Pack pack = (Pack) packs.get(rowIndex);
-			switch(columnIndex)
-			{
-				case 0:
-					int val = 0;
-					if (pack.required)
-					{
-						val = -1;
-					}
-					else
-					{
-						val = (packsToInstall.contains(pack) ? 1 : 0 );
-					}
-					return new Integer( val );
+        /*
+         * @see TableModel#getValueAt(int, int)
+         */
+        public Object getValueAt(int rowIndex, int columnIndex)
+        {
+            Pack pack = (Pack) packs.get(rowIndex);
+            switch(columnIndex)
+            {
+                case 0:
+                    int val = 0;
+                    if (pack.required)
+                    {
+                        val = -1;
+                    }
+                    else
+                    {
+                        val = (packsToInstall.contains(pack) ? 1 : 0 );
+                    }
+                    return new Integer( val );
 
-				case 1:
-					return pack.name;
+                case 1:
+                    return pack.name;
 
-				case 2:
-					return Pack.toByteUnitsString((int)pack.nbytes);
+                case 2:
+                    return Pack.toByteUnitsString((int)pack.nbytes);
 
-				default :
-					return null;
-			}
-		}
+                default :
+                    return null;
+            }
+        }
 
-		/*
-		 * @see TableModel#setValueAt(Object, int, int)
-		 */
-		public void setValueAt(Object aValue, int rowIndex, int columnIndex)
-		{
-			if (columnIndex == 0)
-			{
-				if (aValue instanceof Integer)
-				{
-				    Pack pack = (Pack) packs.get(rowIndex);
-					if (((Integer)aValue).intValue() == 1)
-					{
-				      packsToInstall.add(pack);
-				      bytes += pack.nbytes;
-				    }
-				    else
-				    {
-				      packsToInstall.remove(pack);
-				      bytes -= pack.nbytes;
-				    }
-				    showSpaceRequired();
-				}
-			}
-		}
-	}
-	/**
-	 * @see javax.swing.event.ListSelectionListener#valueChanged(javax.swing.event.ListSelectionEvent)
-	 */
-	public void valueChanged(ListSelectionEvent e)
-	{
-		int i = packsTable.getSelectedRow();
-		if (i >= 0)
-		{
-			Pack pack = (Pack) idata.availablePacks.get(i);
-			descriptionArea.setText(pack.description);
-		}
-	}
+        /*
+         * @see TableModel#setValueAt(Object, int, int)
+         */
+        public void setValueAt(Object aValue, int rowIndex, int columnIndex)
+        {
+            if (columnIndex == 0)
+            {
+                if (aValue instanceof Integer)
+                {
+                    Pack pack = (Pack) packs.get(rowIndex);
+                    if (((Integer)aValue).intValue() == 1)
+                    {
+                      packsToInstall.add(pack);
+                      bytes += pack.nbytes;
+                    }
+                    else
+                    {
+                      packsToInstall.remove(pack);
+                      bytes -= pack.nbytes;
+                    }
+                    showSpaceRequired();
+                }
+            }
+        }
+    }
+    /**
+     * @see javax.swing.event.ListSelectionListener#valueChanged(javax.swing.event.ListSelectionEvent)
+     */
+    public void valueChanged(ListSelectionEvent e)
+    {
+        int i = packsTable.getSelectedRow();
+        if (i >= 0)
+        {
+            Pack pack = (Pack) idata.availablePacks.get(i);
+            descriptionArea.setText(pack.description);
+        }
+    }
 
     static class CheckBoxEditorRenderer extends AbstractCellEditor implements TableCellRenderer,TableCellEditor,ActionListener
     {
-    	private JCheckBox display;
-		public CheckBoxEditorRenderer(boolean useAsEditor)
-		{
-		    display = new JCheckBox();
-		    display.setHorizontalAlignment(JLabel.CENTER);
-		    if (useAsEditor) display.addActionListener(this);
+        private JCheckBox display;
+        public CheckBoxEditorRenderer(boolean useAsEditor)
+        {
+            display = new JCheckBox();
+            display.setHorizontalAlignment(JLabel.CENTER);
+            if (useAsEditor) display.addActionListener(this);
 
-		}
+        }
 
         public Component getTableCellRendererComponent(JTable table, Object value,
-						       boolean isSelected, boolean hasFocus, int row, int column) {
-	    if (isSelected) {
-	        display.setForeground(table.getSelectionForeground());
-	        display.setBackground(table.getSelectionBackground());
-	    }
-	    else {
-	        display.setForeground(table.getForeground());
-	        display.setBackground(table.getBackground());
-	    }
-	    	int state = ((Integer)value).intValue();
+                               boolean isSelected, boolean hasFocus, int row, int column) {
+        if (isSelected) {
+            display.setForeground(table.getSelectionForeground());
+            display.setBackground(table.getSelectionBackground());
+        }
+        else {
+            display.setForeground(table.getForeground());
+            display.setBackground(table.getBackground());
+        }
+            int state = ((Integer)value).intValue();
             display.setSelected((value != null && Math.abs(state) == 1));
             display.setEnabled(state >= 0);
             return display;
         }
-		/**
-		 * @see javax.swing.table.TableCellEditor#getTableCellEditorComponent(javax.swing.JTable, java.lang.Object, boolean, int, int)
-		 */
-		public Component getTableCellEditorComponent(
-			JTable table,
-			Object value,
-			boolean isSelected,
-			int row,
-			int column)
-		{
-			return getTableCellRendererComponent(table, value, isSelected, false, row, column);
+        /**
+         * @see javax.swing.table.TableCellEditor#getTableCellEditorComponent(javax.swing.JTable, java.lang.Object, boolean, int, int)
+         */
+        public Component getTableCellEditorComponent(
+            JTable table,
+            Object value,
+            boolean isSelected,
+            int row,
+            int column)
+        {
+            return getTableCellRendererComponent(table, value, isSelected, false, row, column);
         }
 
-		public Object getCellEditorValue()
-		{
-			return new Integer(display.isSelected() ? 1 : 0);
-		}
+        public Object getCellEditorValue()
+        {
+            return new Integer(display.isSelected() ? 1 : 0);
+        }
 
 
-		public void actionPerformed(ActionEvent e)
-		{
-			stopCellEditing();
-		}
+        public void actionPerformed(ActionEvent e)
+        {
+            stopCellEditing();
+        }
     }
 }
