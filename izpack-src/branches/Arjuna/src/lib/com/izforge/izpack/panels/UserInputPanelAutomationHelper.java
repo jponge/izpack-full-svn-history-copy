fad3e16af0066848d36ca3fb75565b69f3798b5f
tschwarze <tschwarze@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2003-05-20 15:28:17 +0000

    - add default constructor to make automated install work - sourcecode reformatting (remove tabs)
    git-svn-id: http://svn.codehaus.org/izpack@418 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- izpack-src/branches/Arjuna/src/lib/com/izforge/izpack/panels/UserInputPanelAutomationHelper.java
+++ izpack-src/branches/Arjuna/src/lib/com/izforge/izpack/panels/UserInputPanelAutomationHelper.java
@@ -32,6 +32,7 @@ import java.util.Iterator;
 
 import com.izforge.izpack.installer.PanelAutomation;
 import com.izforge.izpack.installer.AutomatedInstallData;
+import com.izforge.izpack.util.Debug;
 
 /**
  *  Functions to support automated usage of the UserInputPanel
@@ -41,107 +42,113 @@ import com.izforge.izpack.installer.AutomatedInstallData;
  */
 public class UserInputPanelAutomationHelper implements PanelAutomation
 {
-	// ------------------------------------------------------
-	// automatic script section keys
-	// ------------------------------------------------------
-	private static final String AUTO_KEY_USER_INPUT = "userInput";
-	private static final String AUTO_KEY_ENTRY = "entry";
+  // ------------------------------------------------------
+  // automatic script section keys
+  // ------------------------------------------------------
+  private static final String AUTO_KEY_USER_INPUT = "userInput";
+  private static final String AUTO_KEY_ENTRY = "entry";
 
-	// ------------------------------------------------------
-	// automatic script keys attributes
-	// ------------------------------------------------------
-	private static final String AUTO_ATTRIBUTE_KEY = "key";
-	private static final String AUTO_ATTRIBUTE_VALUE = "value";
+  // ------------------------------------------------------
+  // automatic script keys attributes
+  // ------------------------------------------------------
+  private static final String AUTO_ATTRIBUTE_KEY = "key";
+  private static final String AUTO_ATTRIBUTE_VALUE = "value";
 
-	// ------------------------------------------------------
-	// String-String key-value pairs
-	// ------------------------------------------------------
-	private Map entries;
+  // ------------------------------------------------------
+  // String-String key-value pairs
+  // ------------------------------------------------------
+  private Map entries;
 
-	/**
-	 *
-	 * @param entries String-String key-value pairs representing the state of the Panel
-	 */
-	public UserInputPanelAutomationHelper(Map entries)
-	{
-		this.entries = entries;
-	}
+  public UserInputPanelAutomationHelper ()
+  {
+    this.entries = null;
+  }
 
-	/**
-	 * Serialize state to XML and insert under panelRoot.
-	 *
-	 * @param idata The installation data.
-	 * @param panelRoot The XML root element of the panels blackbox tree.
-	 */
-	public void makeXMLData(AutomatedInstallData idata, XMLElement panelRoot)
-	{
-		XMLElement userInput;
-		XMLElement dataElement;
+  /**
+   *
+   * @param entries String-String key-value pairs representing the state of the Panel
+   */
+  public UserInputPanelAutomationHelper(Map entries)
+  {
+    this.entries = entries;
+  }
 
-		// ----------------------------------------------------
-		// add the item that combines all entries
-		// ----------------------------------------------------
-		userInput = new XMLElement(AUTO_KEY_USER_INPUT);
-		panelRoot.addChild(userInput);
+  /**
+   * Serialize state to XML and insert under panelRoot.
+   *
+   * @param idata The installation data.
+   * @param panelRoot The XML root element of the panels blackbox tree.
+   */
+  public void makeXMLData(AutomatedInstallData idata, XMLElement panelRoot)
+  {
+    XMLElement userInput;
+    XMLElement dataElement;
 
-		// ----------------------------------------------------
-		// add all entries
-		// ----------------------------------------------------
-		Iterator keys = entries.keySet().iterator();
-		while (keys.hasNext())
-		{
-			String key = (String) keys.next();
-			String value = (String) entries.get(key);
+    // ----------------------------------------------------
+    // add the item that combines all entries
+    // ----------------------------------------------------
+    userInput = new XMLElement(AUTO_KEY_USER_INPUT);
+    panelRoot.addChild(userInput);
 
-			dataElement = new XMLElement(AUTO_KEY_ENTRY);
-			dataElement.setAttribute(AUTO_ATTRIBUTE_KEY, key);
-			dataElement.setAttribute(AUTO_ATTRIBUTE_VALUE, value);
+    // ----------------------------------------------------
+    // add all entries
+    // ----------------------------------------------------
+    Iterator keys = entries.keySet().iterator();
+    while (keys.hasNext())
+    {
+      String key = (String) keys.next();
+      String value = (String) entries.get(key);
 
-			userInput.addChild(dataElement);
-		}
-	}
+      dataElement = new XMLElement(AUTO_KEY_ENTRY);
+      dataElement.setAttribute(AUTO_ATTRIBUTE_KEY, key);
+      dataElement.setAttribute(AUTO_ATTRIBUTE_VALUE, value);
 
-	/**
-	 * Deserialize state from panelRoot and set idata variables accordingly.
-	 *
-	 * @param idata The installation data.
-	 * @param panelRoot The XML root element of the panels blackbox tree.
-	 */
-	public void runAutomated(AutomatedInstallData idata, XMLElement panelRoot)
-	{
-		XMLElement userInput;
-		XMLElement dataElement;
-		String variable;
-		String value;
+      userInput.addChild(dataElement);
+    }
+  }
 
-		// ----------------------------------------------------
-		// get the section containing the user entries
-		// ----------------------------------------------------
-		userInput = panelRoot.getFirstChildNamed(AUTO_KEY_USER_INPUT);
+  /**
+   * Deserialize state from panelRoot and set idata variables accordingly.
+   *
+   * @param idata The installation data.
+   * @param panelRoot The XML root element of the panels blackbox tree.
+   */
+  public void runAutomated(AutomatedInstallData idata, XMLElement panelRoot)
+  {
+    XMLElement userInput;
+    XMLElement dataElement;
+    String variable;
+    String value;
 
-		if (userInput == null)
-		{
-			return;
-		}
+    // ----------------------------------------------------
+    // get the section containing the user entries
+    // ----------------------------------------------------
+    userInput = panelRoot.getFirstChildNamed(AUTO_KEY_USER_INPUT);
 
-		Vector userEntries = userInput.getChildrenNamed(AUTO_KEY_ENTRY);
+    if (userInput == null)
+    {
+      return;
+    }
 
-		if (userEntries == null)
-		{
-			return;
-		}
+    Vector userEntries = userInput.getChildrenNamed(AUTO_KEY_ENTRY);
 
-		// ----------------------------------------------------
-		// retieve each entrie and substitute the associated
-		// variable
-		// ----------------------------------------------------
-		for (int i = 0; i < userEntries.size(); i++)
-		{
-			dataElement = (XMLElement) userEntries.elementAt(i);
-			variable = dataElement.getAttribute(AUTO_ATTRIBUTE_KEY);
-			value = dataElement.getAttribute(AUTO_ATTRIBUTE_VALUE);
+    if (userEntries == null)
+    {
+      return;
+    }
 
-			idata.getVariableValueMap().setVariable(variable, value);
-		}
-	}
+    // ----------------------------------------------------
+    // retieve each entry and substitute the associated
+    // variable
+    // ----------------------------------------------------
+    for (int i = 0; i < userEntries.size(); i++)
+    {
+      dataElement = (XMLElement) userEntries.elementAt(i);
+      variable = dataElement.getAttribute(AUTO_ATTRIBUTE_KEY);
+      value = dataElement.getAttribute(AUTO_ATTRIBUTE_VALUE);
+
+      Debug.trace ("UserInputPanel: setting variable "+variable+" to "+value);
+      idata.getVariableValueMap().setVariable(variable, value);
+    }
+  }
 }
