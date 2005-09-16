c30d82e343d2dd50f6d51b276043dc805493edec
(no author) <(no author)@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2005-09-16 00:41:47 +0000

    This commit was manufactured by cvs2svn to create tag 'milestone-3-8-0-M2'.
    git-svn-id: http://svn.codehaus.org/izpack@1356 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ izpack-src/tags/milestone-3-8-0-M2/src/lib/com/izforge/izpack/util/UnixGroupProcessor.java
@@ -0,0 +1,56 @@
+/* 
+*  Copyright (C) 2004 Thorsten Kamann
+*
+*  File :               UnixGroupProcessor.java
+*  Description :       Retrieves a list of the current groups 
+*  Author's email :     thorsten.kamann@planetes.de
+*  Author's Website :   http://www.izforge.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*     http://www.apache.org/licenses/LICENSE-2.0
+*     
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.izforge.izpack.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import com.izforge.izpack.panels.ProcessingClient;
+import com.izforge.izpack.panels.Processor;
+
+/**
+ * @author thorsten-kamann
+ */
+public class UnixGroupProcessor implements Processor {
+
+	public String process (ProcessingClient client){
+		String retValue = "";
+		String filepath = "/etc/group";
+		BufferedReader reader = null;
+		String line = "";
+				
+		try{
+			reader = new BufferedReader(new FileReader(filepath));
+			while ((line = reader.readLine()) != null){
+				retValue += line.substring(0, line.indexOf(":"))+":";
+			}
+			if (retValue.endsWith(":")){
+				retValue = retValue.substring(0, retValue.length()-1);
+			}			
+		}catch (Exception ex){
+			retValue = "";
+		}
+		
+		return retValue;
+	}
+
+}
