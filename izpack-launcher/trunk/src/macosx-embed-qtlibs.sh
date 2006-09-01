81b29d6461ecd05d7df9c760d68790398a651c1b
jponge <jponge@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2006-09-01 09:58:11 +0000

    Create lighter MacOS X bundles.
    git-svn-id: http://svn.codehaus.org/izpack@1555 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- izpack-launcher/trunk/src/macosx-embed-qtlibs.sh
+++ izpack-launcher/trunk/src/macosx-embed-qtlibs.sh
@@ -9,6 +9,9 @@ mkdir launcher.app/Contents/Frameworks
 cp -R $1/QtCore.framework launcher.app/Contents/Frameworks
 cp -R $1/QtGui.framework launcher.app/Contents/Frameworks
 
+find launcher.app -name '*_debug' | xargs rm
+find launcher.app -name 'Headers' | xargs rm -rf
+
 install_name_tool -id @executable_path/../Frameworks/QtCore.framework/Versions/4.0/QtCore \
 launcher.app/Contents/Frameworks/QtCore.framework/Versions/4.0/QtCore
 
@@ -26,3 +29,5 @@ launcher.app/Contents/MacOs/launcher
 install_name_tool -change path/to/Qt/lib/QtCore.framework/Versions/4.0/QtCore \
 @executable_path/../Frameworks/QtCore.framework/Versions/4.0/QtCore \
 launcher.app/Contents/Frameworks/QtGui.framework/Versions/4.0/QtGui
+
+tar cjf ../dist/launcher.app-macosx-universal.tar.bz2 launcher.app/
