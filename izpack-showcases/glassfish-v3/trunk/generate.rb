c6061ae0d1854affa6a2ea083d567adef6feda52
jponge <jponge@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2008-09-03 14:08:54 +0000

    Initial import.
    git-svn-id: http://svn.codehaus.org/izpack@2323 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

---
+++ izpack-showcases/glassfish-v3/trunk/generate.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+#
+# Copyright (c) 2007, 2008 Julien Ponge. All rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+require 'fileutils'
+
+if (ARGV.length != 1)
+  puts "Syntax: generate <IzPack home directory>"
+  exit(-1)
+end
+
+izpack_home = ARGV[0]
+
+puts "Compiling the installers..."
+system "#{izpack_home}/bin/compile glassfish-install.xml -h #{izpack_home} -o glassfish-v3-latest-install.jar -c deflate -l 9"
+
+puts "Done"
