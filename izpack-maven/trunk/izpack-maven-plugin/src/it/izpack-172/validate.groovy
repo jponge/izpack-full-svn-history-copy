5bc70574770474611a99142c6be6f2ab21e67b54
dantran <dantran@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2008-10-04 03:07:46 +0000

    more validation cases for izpack-172
    git-svn-id: http://svn.codehaus.org/izpack@2343 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- izpack-maven/trunk/izpack-maven-plugin/src/it/izpack-172/validate.groovy
+++ izpack-maven/trunk/izpack-maven-plugin/src/it/izpack-172/validate.groovy
@@ -3,5 +3,7 @@ assert new File(basedir, 'target/izpack/install.xml').exists();
 
 content = new File(basedir,'target/izpack/install.xml').text;
 
+assert content.contains( 'name="org.codehaus.izpack"' )
+assert content.contains( 'email="org.codehaus.izpack@domain.com"' )
 assert ! content.contains( '@{app.version.static}' )
 assert content.contains( '1.5.6' )
