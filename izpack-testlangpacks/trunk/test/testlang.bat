ea8217f3e0c2bbc3c9d541e95e74196a7e378d64
avoutila <avoutila@7d736ef5-cfd4-0310-9c9a-b52d5c14b761>
2009-02-05 18:44:11 +0000

    TestLangPacks: Removed old test directory. New unit test directory should be used. (IZPACK-133)
    git-svn-id: http://svn.codehaus.org/izpack@2540 7d736ef5-cfd4-0310-9c9a-b52d5c14b761

--- izpack-testlangpacks/trunk/test/testlang.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem
-rem Testing and developing purposes
-rem Location: IzPack-TestLangPacks\test
-
-echo For testing and developing purposes.
-java -jar ..\bin\TestLangPacks.jar %1 %2 %3
--- izpack-testlangpacks/trunk/test/testlang.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Testing and developing purposes
-# Location: IzPack-TestLangPacks/test
-
-print For testing and developing purposes.
-java -jar ../bin/TestLangPacks.jar $1 $2 $3
